c0207273ed6ca19537674b73ae78a88630f8366f
Gabriel Oliveira <118837823+Gabriel-Oliveiraa@users.noreply.github.com>
2024-12-26 20:48:06 -0300

    Add files via upload

---
+++ src/Enumeracoes&Composicao.java
@@ -0,0 +1,13 @@
+/*
+###1 Enumerações
+
+É um tipo especial que serve para especificar de forma literal um
+conjunto de constantes relacionadas
+
+Palavra chave em Java: enum
+
+Vantagem: melhor semântica, código mais legível e auxiliado pelo
+compilador
+
+
+ */
