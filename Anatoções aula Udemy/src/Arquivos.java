30a88ee4a055806a8fc04920b5e58178dbcc0c21
Gabriel Oliveira <118837823+Gabriel-Oliveiraa@users.noreply.github.com>
2024-12-28 14:30:10 -0300

    Add files via upload

---
+++ Anatoções aula Udemy/src/Arquivos.java
@@ -0,0 +1,17 @@
+/*
+###1 Lendo arquivo texto com classes File e Scanner
+
+FileReader e BufferedReader
+
+###2 Bloco try-with-resources
+
+É um bloco try que declara um ou mais recursos, e garante que esses
+recursos serão fechados ao final do bloco
+
+###3 Escrevendo arquivo texto com classes File e Scanner
+
+Cria/recria o arquivo: new FileWriter(path)
+Acrescenta ao arquivo existente: new FileWriter(path, true)
+
+BufferedWriter (mais rápido)
+ */
